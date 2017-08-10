f39191bd1cf69638ac699251c1988291424a3e93
a.dechiara <a.dechiara@NB-ADECHIARA.mainsoft.local>
2017-08-10 12:35:33 +0200

    import node_module

---
+++ index.js
@@ -0,0 +1,14 @@
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+/**
+ * @module
+ * @description
+ * Entry point for all public APIs of the core package.
+ */
+export { InserimentordineComponent } from './src/app/component/ordine/inserimento/inserimentordine';
+//# sourceMappingURL=index.js.map
